0688c209faff096d08189071ce99b37824c0054f
pawelkruszko <89316551+pawelkruszko@users.noreply.github.com>
2022-01-26 18:20:48 +0100

    Menu update

--- calculator.py
+++ calculator.py
@@ -1,18 +1,40 @@
-# create simple calculator based on float numbers
 import operator
 
+menu = """Two Numbers Calculator:
+operators:
+  +
+  -
+  *
+  /
+
+MENU:
+1. calculator
+2. exit
+"""
+print(menu)
 
 while True:
+
+    menu_option = int(input("(type menu option number to continue): "))
+
+    if menu_option not in (1, 2):
+        print("Wrong menu option selected, try again.")
+        continue
+
+    if menu_option == 2:
+        print("Exiting...")
+        break
+
     # input numbers
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
-
     # input sign
-    signs = {"+": operator.add,
-             "-": operator.sub,
-             "*": operator.mul,
-             "/": operator.itruediv
-             }
+    signs = {
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul,
+        "/": operator.truediv
+    }
 
     sign = input(f"Input operator {signs.keys()}: ")
 
@@ -22,4 +44,3 @@ while True:
         print("Invalid operator")
     else:
         print(operation(num1, num2))
-
